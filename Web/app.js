03ac8a88bd538a89c2799be918bb9c3f60eb1923
LeonNegrete <leon.negrete.dev@gmail.com>
2022-09-18 15:24:52 -0300

    El home completo, solo falta la etiqueta de enlace en cada articulo :)

--- Web/app.js
+++ Web/app.js
@@ -7,10 +7,10 @@ const path = require('path');
 const PORT = 3030;
 
 
-app.listen(PORT,()=>{console.log(`Servidor corriendo en ${PORT}...`)});
+app.listen(PORT, () => { console.log(`Servidor corriendo en ${PORT}...`) });
 
-app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/home',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/home.html'))
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
 })
