1275602c0a3b3bc230e0ea8cbc423fcd1aa08ade
LeonNegrete <leon.negrete.dev@gmail.com>
2022-09-28 23:32:28 -0300

    reorganizando las vistas en carpetas products y users asi como tambien corrigiendo las rutas que se refieren a dichas vistas (en el main controller y cuando se invocan los partials (../))

--- Web/src/controllers/mainController.js
+++ Web/src/controllers/mainController.js
@@ -5,27 +5,27 @@ const path = require('path');
     },
 
     home: (req, res) => {
-        res.render(path.join(__dirname, '../views/home.ejs'))
+        res.render(path.join(__dirname, '../views/products/home.ejs'))
     },
 
     detalle: (req, res)=>{
-        res.render(path.join(__dirname, '../views/detalle.ejs'))
+        res.render(path.join(__dirname, '../views/products/detalle.ejs'))
     },
     
     design: (req, res)=>{
-        res.render(path.join(__dirname, '../views/design.ejs'))
+        res.render(path.join(__dirname, '../views/products/design.ejs'))
     },
 
     login: (req, res)=>{
-        res.render(path.join(__dirname, '../views/login.ejs'))
+        res.render(path.join(__dirname, '../views/users/login.ejs'))
     },
 
     register:(req, res)=>{
-        res.render(path.join(__dirname, '../views/register.ejs'))
+        res.render(path.join(__dirname, '../views/users/register.ejs'))
     },
 
     carrito:(req, res)=>{
-        res.render(path.join(__dirname, '../views/shop-car.ejs'))
+        res.render(path.join(__dirname, '../views/products/shop-car.ejs'))
     }
 
 }
