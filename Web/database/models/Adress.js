d67bf5d647bf04ab52e49ad06994b8a3d56e585c
LeonNegrete <leon.negrete.dev@gmail.com>
2022-12-14 23:39:16 -0300

    Seteando sequelize

---
+++ Web/database/models/Adress.js
@@ -0,0 +1,8 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) =>{
+    
+
+    const Adress = sequelize.define(alias, cols,config)
+}
