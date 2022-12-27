10cb8036060e055afcddcefcae3466d5f8265e4d
GeraLZ <gera.lz2305@gmail.com>
2022-12-27 13:18:22 -0600

    Pruebas Sequelize 1

---
+++ Web/database/models/Users.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Users';
+    let cols = {
+        user_id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_nick: {
+            type: dataTypes.STRING
+        },
+        user_name: {
+            type: dataTypes.STRING
+        },
+        user_pass: {
+            type: dataTypes.STRING
+        },
+        user_email: {
+            type: dataTypes.STRING
+        },
+        user_admin: {
+            type: dataTypes.INTEGER
+        }
+    };
+    let config = {
+        tableName: 'Users',
+        timestamps: false
+    };
+    const Users = sequelize.define(alias, cols, config)
+
+    Users.associate = (models)=> {
+        Users.hasMany(models.Shirts, { 
+            as: "Shirts", 
+            foreignKey: "user_id"
+        })
+    }
+    return Users
+}
