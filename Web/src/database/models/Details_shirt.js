ead2431c0a75f9ced64aa77c4cac76a9ee857e31
GeraLZ <gera.lz2305@gmail.com>
2023-01-04 18:03:54 -0600

    Sequelize details

---
+++ Web/src/database/models/Details_shirt.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Detail_shirt';
+    let cols = {
+        detail_id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        shirt_size: {
+            type: dataTypes.STRING
+        },
+        shirt_stock: {
+            type: dataTypes.INTEGER
+        }
+    };
+    let config = {
+        tableName: 'Details_shirt',
+        timestamps: false
+    };
+    const Details_shirt = sequelize.define(alias, cols, config)
+
+    Details_shirt.associate =  (models) => {
+        Shirts.belongsTo(models.Users, { 
+            as: "Shirts",
+            foreignKey: "shirt_id"
+        })
+
+    }
+    return Details_shirt
+}
