66654d713560b8d48de77b097896f5b54db8d800
GeraLZ <gera.lz2305@gmail.com>
2023-01-04 18:23:33 -0600

    Sequelize Cart y timestamp sequelize v2

---
+++ Web/src/database/models/Cart.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Cart';
+    let cols = {
+        cart_id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        cart_total: {
+            type: dataTypes.DOUBLE
+        },
+        cart_created_time: {
+            //Timestamp sequelize version 2 para pruebas
+            type: dataTypes.TIMESTAMP,
+            defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+            allowNull: false
+        }
+
+    };
+    let config = {
+        tableName: 'Cart',
+        timestamps: false,
+       // createdAt: "cart_created_time"
+    };
+    const Cart = sequelize.define(alias, cols, config)
+
+    Cart.associate =  (models) => {
+        Cart.belongsTo(models.Users, { 
+            as: "Users",
+            foreignKey: "user_id"
+        })
+
+    }
+    return Cart
+}
