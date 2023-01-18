a856fa57ca179b9d75d1741a72cb3a8ed3a86613
LeonNegrete <leon.negrete.dev@gmail.com>
2023-01-17 22:47:11 -0300

    Create, detail, home con bdd

--- Web/src/controllers/productController.js
+++ Web/src/controllers/productController.js
@@ -2,6 +2,7 @@ const path = require('path');
 const products = require('../data/products.json');
 const fs = require('fs');
 const db = require('../database/models');
+/* const { removeTicks } = require('sequelize/types/utils'); */ // me pa que esto no tiene que estar aca
 const sequelize = db.sequelize;
 const productController = {
     //Funcion destinada a traer el JSON 'Products' y transformarlo en un objeto literal
@@ -19,25 +20,32 @@ const productController = {
         producto.image = newData.image || producto.image;
     },
 
-    hayStock: (arrayDeTalles) => {
-        let stock = arrayDeTalles.sizesQuantity.every((e) => (e == 0)); //evalua si todos los elementos del array que contiene el stock estan en cero
-        if (stock) {
-            return false //Si estan en cero retorna false (No hay stock)
-        } else {
-            return true //Caso contrario retorna true (hay stock)
-        }
+    hayStock: async(Remera) => {
+        let talles = await db.Details_shirt.findAll({
+            where: {
+                shirt_id : Remera.dataValues.shirt_id
+            }
+        })
+        talles.forEach(talle => {
+            let stock = false
+            if (talle.dataValues.shirt_stock > 0){
+                return true
+            }
+            return stock;
+        });
     },
 
-    home: (req, res) => {
+    home: async(req, res) => {
         let session = req.session;
-        let productsArray = productController.productsArr()
+        let productsArray = await db.Shirts.findAll()
+        console.log(productsArray)
         let soldOut = [];
         let onSale = [];
-        for (const e of productsArray.products) { //itera el array pasando por cada remera
-            if (productController.hayStock(e)) {
-                onSale.push(e) //Si estan en cero se los agrega al array de vendidos
+        for (const remera of productsArray) { //itera el array pasando por cada remera
+            if (productController.hayStock(remera)) {
+                onSale.push(remera) 
             } else {
-                soldOut.push(e) //Caso contrario se los agrega al array de disponibles
+                soldOut.push(remera) 
             }
         }
         res.render(path.join(__dirname, '../views/products/home.ejs'), { onSale, soldOut, session }) //Se exportan los arrays de vendidos y disponibles
@@ -63,16 +71,6 @@ const productController = {
         }catch(err){
             console.log(err);
         }
-        
-
-       /*  let sizesList = productController.productsArr().sizesList; //Se trae el array con la lista de talles que puede tener cada remera
-        let productsArray = productController.productsArr().products;//Se trae el array con la lista que contiene a todas las remeras
-        let idP = req.params.id; //se trae a la id ingresada en la url
-        let correctIndex = productsArray.indexOf(productsArray.find((e) => { //en caso de que el indice no sea el mismo que la id (Cosa que puede pasar despues de crear y borrar varios productos)
-            return e.id === idP
-        })) */
-
-        /* res.render(path.join(__dirname, '../views/products/detalle.ejs'), { correctIndex, productsArray, sizesList, session }) */
     },
 
     design: (req, res) => {
@@ -91,8 +89,6 @@ const productController = {
     },
 
     admCreatePost: async (req, res) => {
-        //console.log(req.body)
-        //console.log(req.session)
         try{
             let shirtToCreate = await db.Shirts.findOne({ where : { shirt_name : req.body.name_product } })
             
@@ -122,18 +118,13 @@ const productController = {
                 let shirtCreated = await db.Shirts.findOne({ where : { shirt_name : req.body.name_product } })
     
                 for (let talla in stock){
-                    //console.log(`${talla}: ${stock[talla]}`)
                     await db.Details_shirt.create({
                         shirt_size: talla,
                         shirt_stock: stock[talla],
                         shirt_id: shirtCreated.shirt_id
                     })
                 } 
-    
                 res.redirect(`/products/${shirtCreated.shirt_id}`);
-                
-                /* console.log("Las tallas ingresadas fueron.-")
-                console.log(tallas) */
             }
         
         }catch(err){
@@ -159,15 +150,6 @@ const productController = {
             editedData.sizesQuantity = undefined //Cuando se sube un undefined, en la funcion edictExistingProduct, se conservara el valor actual
         } else { editedData.sizesQuantity = [req.body.xs, req.body.s, req.body.m, req.body.l, req.body.xl, req.body.xxl]; } //Si se hizo un cambio se subira   
 
-
-
-
-        /*  if ((req.body.xs === '') && (req.body.s === '') && (req.body.m === '') && (req.body.l === '') && (req.body.xl === '') && (req.body.xxl === '')) { //Condicional necesario para no sobreescribir los datos de stock con un 'null' cada vez que se hace una edicion donde no se especifica el stock (la idea es que se asume que si no se especifica un dato en el formulario es debido a que no se quiere cambiarlo)
-             editedData.sizesQuantity = undefined //Cuando se sube un undefined, en la funcion edictExistingProduct, se conservara el valor actual
-         } else { editedData.sizesQuantity = [req.body.xs, req.body.s, req.body.m, req.body.l, req.body.xl, req.body.xxl]; } //Si se hizo un cambio se subira    */
-        console.log(req.file)
-        /* editedData.image = (req.file.filename); */
-        console.log(editedData)
         let parsedJSON = productController.productsArr(); //Almacena el JSON convertido en objeto dentro de una variable
         let elementToEdit = parsedJSON.products.find((e) => { //Busca el producto de manera que lo encuentre satisfactoriamente aunque el indice del array no sea el mismo que su id
             return e.id === idP
